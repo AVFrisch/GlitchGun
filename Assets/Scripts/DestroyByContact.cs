71ccda24c6e47756b2affff0ab6b8e483b50c559
AVFrisch <ahmed.v.frisch@gmail.com>
2019-02-24 22:38:02 -0500

    Asteroids spawn in waves and explosion effects delete themselves after 2 seconds

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -16,7 +16,7 @@ public class DestroyByContact : MonoBehaviour
         if (other.tag == "Player")
         {
             Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
-            gameController.GameOver();
+            //gameController.GameOver();
         }
         Destroy(other.gameObject);
         Destroy(gameObject);

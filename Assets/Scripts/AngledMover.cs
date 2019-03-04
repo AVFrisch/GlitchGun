5b779c5f1e1a1a80fed3c5bd3fb24de8270b9167
Frisch <avfrisch@ads.iu.edu>
2019-03-04 11:30:14 -0500

    Bolts now face and move in the correct direction about 50% of the time, they otherwise shoot straight up

--- Assets/Scripts/AngledMover.cs
+++ Assets/Scripts/AngledMover.cs
@@ -7,12 +7,30 @@ public class AngledMover : MonoBehaviour
 
     void Start()
     {
-        Vector3 v3T = Input.mousePosition;
-        v3T.z = Mathf.Abs(Camera.main.transform.position.y - transform.position.y);
-        v3T = Camera.main.ScreenToWorldPoint(v3T);
-        transform.LookAt(v3T);
+       
+        
+        
+        //v3T.z = Mathf.Abs(Camera.main.transform.position.y - transform.position.y);
+        //v3T = Camera.main.ScreenToWorldPoint(v3T);
+        //transform.LookAt(v3T);
 
+
+    }
+
+    //
+    void Update()
+    {
+
+        Vector3 v3T = transform.forward * speed;
         GetComponent<Rigidbody>().velocity = v3T;
 
+
+
+        //    Vector3 v3T = Input.mousePosition;
+        //    v3T.z = Mathf.Abs(Camera.main.transform.position.y - transform.position.y);
+        //    v3T = Camera.main.ScreenToWorldPoint(v3T);
+        //    transform.LookAt(v3T);
+
+        //    GetComponent<Rigidbody>().velocity = v3T;
     }
 }

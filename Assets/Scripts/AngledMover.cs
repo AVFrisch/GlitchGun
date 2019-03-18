12e1922889e3f7f0721ae6f650aed0ee4ebf540c
Frisch <avfrisch@ads.iu.edu>
2019-03-18 12:10:36 -0400

    Burst shot added with paramters for float and spread

--- Assets/Scripts/AngledMover.cs
+++ Assets/Scripts/AngledMover.cs
@@ -4,21 +4,22 @@ using System.Collections;
 public class AngledMover : MonoBehaviour
 {
     public float speed;
+    public float spread = 15;
 
     void Start()
     {
-
-        if (GetComponent<Rigidbody>().name.Contains("Burst")){
-            GetComponent<Rigidbody>().transform.Rotate(0f, 10f, 0f, Space.Self);
-        }
-
-        print(GetComponent<Rigidbody>().name);
 
         Vector3 v3T = Input.mousePosition;
         v3T.z = Mathf.Abs(Camera.main.transform.position.y - transform.position.y);
         v3T = Camera.main.ScreenToWorldPoint(v3T);
         transform.LookAt(v3T);
 
+        if (GetComponent<Rigidbody>().name.Contains("Burst"))
+        {
+            float randangle = Random.Range(-spread, spread);
+            GetComponent<Rigidbody>().transform.Rotate(0f, randangle, 0f, Space.World);
+            print("rotated");
+        }
 
     }
 
